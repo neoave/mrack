c41f93b2e3dc736563dce2bce59c1bcb60a9c58a
Armando Neto <abiagion@redhat.com>
2020-07-24 16:55:17 -0300

    Add basic logging mechanism
    Configure a logger to be used in the whole application.
    
    If user pass `--debug` to aiohabit log level is changed to DEBUG.
    
    Signed-off-by: Armando Neto <abiagion@redhat.com>

--- src/aiohabit/__init__.py
+++ src/aiohabit/__init__.py
@@ -1,1 +1,21 @@
 """aiohabit library."""
+import logging
+
+logger = logging.getLogger(__name__)
+# Default level used by file_handler
+logger.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(message)s")
+console_formatter = logging.Formatter("%(name)s %(message)s")
+
+file_handler = logging.FileHandler("aiohabit.log")
+file_handler.setFormatter(file_formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(console_formatter)
+
+# Default level for console can be overridden by passing --debug to the executable
+console_handler.setLevel(logging.INFO)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
