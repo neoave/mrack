be8d5fadf03df53d61f55321314dab45219b867b
Tibor Dudlák <tdudlak@redhat.com>
2020-08-05 10:57:51 +0200

    Docs: do not use source/_static directory
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "iohabit"
+project = "mrack"
 copyright = "2020, Red Hat Inc."
 author = "Petr Vobornik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -51,4 +51,4 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
