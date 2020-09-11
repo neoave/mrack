09b3be31af4924ae52dc5ace50b77a9d43f49eb3
Tibor Dudlák <tdudlak@redhat.com>
2020-09-11 14:33:04 +0200

    Release version 0.1.4
    Bug fixing and testing:
    
        Fixed username for beaker machines to be root.
    
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Red Hat Inc."
 author = "Petr Vobornik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.3"
+release = "0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
