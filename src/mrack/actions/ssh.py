neoave/mrack

commit 19d513ba246ef4be02a0ce3d22fbb0faea971ce8

feat: improve logging for openstack and ssh subprocess
Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

src/mrack/utils.py
@@ -221,11 +221,11 @@ def ssh_to_host(
     ssh_key = ssh_key or get_ssh_key(host, meta_host, global_context["config"])
     psw = host.password or password
 
-    my_env = os.environ.copy()
-
     run_args = {
-        "env": my_env,
+        "env": os.environ.copy(),
         "shell": True,
+        "stdout": subprocess.PIPE,
+        "stderr": subprocess.PIPE,
     }
 
     cmd = ["ssh"]
@@ -249,9 +249,16 @@ def ssh_to_host(
 
     cmd = " ".join(cmd)
 
-    logger.info(cmd)
+    logger.debug(f"Running: {cmd}")
     process = subprocess.Popen(cmd, **run_args)
-    process.communicate()
+    std_out, std_err = process.communicate()
+
+    for o_line in std_out.decode().splitlines():
+        logger.debug(f"stdout: {o_line}")
+
+    for e_line in std_err.decode().splitlines():
+        logger.debug(f"stderr: {e_line}")
+
     return process.returncode == 0
 
 

src/mrack/providers/openstack.py
@@ -635,7 +635,7 @@ class OpenStackProvider(Provider):
         timeout_time = start + timedelta(minutes=self.timeout)
 
         # do not check the state immediately, it will take some time
-        logger.debug(f"{uuid}: sleeping for {poll_sleep_initial} seconds")
+        logger.debug(f"{uuid}: sleeping for {poll_sleep_initial:.1f} seconds")
         await asyncio.sleep(poll_sleep_initial)
 
         resp = {}
@@ -657,7 +657,7 @@ class OpenStackProvider(Provider):
                 break
 
             poll_sleep += 0.5  # increase delays to check the longer it takes
-            logger.debug(f"{uuid}: sleeping for {poll_sleep} seconds")
+            logger.debug(f"{uuid}: sleeping for {poll_sleep:.1f} seconds")
             await asyncio.sleep(poll_sleep)
 
         done_time = datetime.now()

commit 1ab510366112f5c29172cc27c7477b3a1a5f6d93

fix: SSH action - do not redirect to PIPEs
SSH action was previously redirected to PIPEs and thus it stopped
working from a user perspective as nothing was visible.

This patch is bringing the original behavior back.

Regression was caused by commit: 19d513ba246ef4be02a0ce3d22fbb0faea971ce8

Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

src/mrack/actions/ssh.py
@@ -84,7 +84,11 @@ class SSH(Action):
             host, global_context
         )
         return utils_ssh_to_host(
-            host, username=username, password=password, ssh_key=ssh_key
+            host,
+            username=username,
+            password=password,
+            ssh_key=ssh_key,
+            interactive=True,
         )
 
     def is_container_env(self, host):

src/mrack/utils.py
@@ -243,6 +243,7 @@ def ssh_to_host(
     password=None,
     ssh_key=None,
     command=None,
+    interactive=False,
 ):
     """SSH to the selected host."""
     psw = host.password or password
@@ -250,9 +251,14 @@ def ssh_to_host(
     run_args = {
         "env": os.environ.copy(),
         "shell": True,
-        "stdout": subprocess.PIPE,
-        "stderr": subprocess.PIPE,
     }
+    if not interactive:
+        run_args.update(
+            {
+                "stdout": subprocess.PIPE,
+                "stderr": subprocess.PIPE,
+            }
+        )
 
     cmd = ["ssh"]
     cmd.extend(["-o", "'StrictHostKeyChecking=no'"])
@@ -279,11 +285,12 @@ def ssh_to_host(
     process = subprocess.Popen(cmd, **run_args)
     std_out, std_err = process.communicate()
 
-    for o_line in std_out.decode().splitlines():
-        logger.debug(f"stdout: {o_line}")
+    if not interactive:
+        for o_line in std_out.decode().splitlines():
+            logger.debug(f"stdout: {o_line}")
 
-    for e_line in std_err.decode().splitlines():
-        logger.debug(f"stderr: {e_line}")
+        for e_line in std_err.decode().splitlines():
+            logger.debug(f"stderr: {e_line}")
 
     return process.returncode == 0
 