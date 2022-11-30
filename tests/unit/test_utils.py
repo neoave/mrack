de0986f4c13b45741cd613345d07f42601c90e46
Tibor Dudlák <tdudlak@redhat.com>
2022-11-30 17:07:46 +0100

    test: Add test for value_to_bool util function
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mrack.utils import get_shortname, get_username
+from mrack.utils import get_shortname, get_username, value_to_bool
 
 
 @pytest.mark.parametrize(
@@ -27,3 +27,41 @@ def test_get_username(
 
     value = get_username(host_win_aws, metahost_win, provisioning_config)
     assert value == "Administrator"
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ("true", True),
+        ("True", True),
+        ("t", True),
+        ("T", True),
+        ("TRUe", True),
+        ("trUE", True),
+        ("TRUE", True),
+        ("1", True),
+        ("false", False),
+        ("False", False),
+        ("FALse", False),
+        ("faLSE", False),
+        ("FALSE", False),
+        ("0", False),
+        ([], False),
+        (["ITEM"], True),
+        ({"A": 9}, True),
+        ({}, False),
+        ((), False),
+        ((1, 2), True),
+        (True, True),
+        (False, False),
+        ("foo.true.baz", ValueError),
+        ("abc false", ValueError),
+        ("true9.", ValueError),
+        ("false1", ValueError),
+    ],
+)
+def test_value_to_bool(value, expected):
+    try:
+        assert value_to_bool(value) == expected
+    except Exception as exc:
+        assert isinstance(exc, expected)
