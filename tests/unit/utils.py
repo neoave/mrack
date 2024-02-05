37df0e4d1bd6cf7c8091867c019b73b9ec28e92a
Lili Nie <lnie@redhat.com>
2024-02-05 09:31:00 -0500

    test: Add test for _get_recipe_info function

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -8,11 +8,23 @@ def test_dir_path():
     return os.path.dirname(os.path.realpath(__file__))
 
 
-def get_data(name, data_dir="data"):
+def get_file_path(name, data_dir="data"):
     path = os.path.join(test_dir_path(), data_dir, name)
+    return path
+
+
+def get_data(name, data_dir="data"):
+    path = get_file_path(name, data_dir)
     with open(os.path.expanduser(path), "r", encoding="utf-8") as file_data:
         data = json.load(file_data)
     return data
+
+
+def get_content(name, data_dir="data"):
+    path = get_file_path(name, data_dir)
+    with open(os.path.expanduser(path), "r", encoding="utf-8") as file_content:
+        content = file_content.read()
+    return content
 
 
 def copy_as_temp(data_path, text=True):
