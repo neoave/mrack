0683fc891c98423d798180ebafc1aeb66ef93ecc
Alexander Bokovoy <abokovoy@redhat.com>
2024-10-10 13:56:21 +0300

    style: Reformat by black

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['m2r2']
+extensions = ["m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -54,4 +54,4 @@ html_theme = "default"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
